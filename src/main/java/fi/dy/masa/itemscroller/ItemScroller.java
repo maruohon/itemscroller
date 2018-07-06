5868446c5447d730da70bce7aa2114f10b84ed45
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2018-07-06 19:12:26 +0300

    Add JAR signing, bump Forge and mappings versions

--- src/main/java/fi/dy/masa/itemscroller/ItemScroller.java
+++ src/main/java/fi/dy/masa/itemscroller/ItemScroller.java
@@ -1,13 +1,15 @@
 package fi.dy.masa.itemscroller;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import fi.dy.masa.itemscroller.config.Configs;
 import fi.dy.masa.itemscroller.proxy.CommonProxy;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION,
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, certificateFingerprint = Reference.FINGERPRINT,
     guiFactory = "fi.dy.masa.itemscroller.config.ItemScrollerGuiFactory",
     updateJSON = "https://raw.githubusercontent.com/maruohon/itemscroller/master/update.json",
     clientSideOnly=true, acceptedMinecraftVersions = "1.12")
@@ -19,13 +21,31 @@ public class ItemScroller
     @SidedProxy(clientSide = "fi.dy.masa.itemscroller.proxy.ClientProxy", serverSide = "fi.dy.masa.itemscroller.proxy.CommonProxy")
     public static CommonProxy proxy;
 
-    public static Logger logger;
+    public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        logger = event.getModLog();
         proxy.registerEventHandlers();
         Configs.loadConfigsFromFile(event.getSuggestedConfigurationFile());
+    }
+
+    @Mod.EventHandler
+    public void onFingerPrintViolation(FMLFingerprintViolationEvent event)
+    {
+        // Not running in a dev environment
+        if (event.isDirectory() == false)
+        {
+            logger.warn("*********************************************************************************************");
+            logger.warn("*****                                    WARNING                                        *****");
+            logger.warn("*****                                                                                   *****");
+            logger.warn("*****   The signature of the mod file '{}' does not match the expected fingerprint!     *****", event.getSource().getName());
+            logger.warn("*****   This might mean that the mod file has been tampered with!                       *****");
+            logger.warn("*****   If you did not download the mod {} directly from Curse/CurseForge,       *****", Reference.MOD_NAME);
+            logger.warn("*****   or using one of the well known launchers, and you did not                       *****");
+            logger.warn("*****   modify the mod file at all yourself, then it's possible,                        *****");
+            logger.warn("*****   that it may contain malware or other unwanted things!                           *****");
+            logger.warn("*********************************************************************************************");
+        }
     }
 }
