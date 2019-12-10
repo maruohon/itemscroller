ebce70da276611e56adab96c81a03d595a8eda06
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2019-12-10 21:36:20 +0200

    Fix a mod init issue

--- src/main/java/fi/dy/masa/itemscroller/ItemScroller.java
+++ src/main/java/fi/dy/masa/itemscroller/ItemScroller.java
@@ -1,11 +1,17 @@
 package fi.dy.masa.itemscroller;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import fi.dy.masa.itemscroller.event.forge.ForgeRenderEventHandler;
 import fi.dy.masa.malilib.event.InitializationHandler;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod(Reference.MOD_ID)
 public class ItemScroller
@@ -14,8 +20,16 @@ public class ItemScroller
 
     public ItemScroller()
     {
-        MinecraftForge.EVENT_BUS.register(new ForgeRenderEventHandler());
-
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientInit);
         InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
+    }
+
+    private void onClientInit(final FMLClientSetupEvent event)
+    {
+        // Make sure the mod being absent on the other network side does not cause
+        // the client to display the server as incompatible
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+
+        MinecraftForge.EVENT_BUS.register(new ForgeRenderEventHandler());
     }
 }
