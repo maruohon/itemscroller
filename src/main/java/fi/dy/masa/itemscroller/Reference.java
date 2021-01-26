5c8e6ee761043f3e5de6bdb8aa9b5fd17dcd0437
Andrews54757 <andrews54757@gmail.com>
2021-01-26 11:50:17 -0500

    Change title

--- src/main/java/fi/dy/masa/itemscroller/Reference.java
+++ src/main/java/fi/dy/masa/itemscroller/Reference.java
@@ -1,8 +1,10 @@
 package fi.dy.masa.itemscroller;
 
+import fi.dy.masa.malilib.util.StringUtils;
+
 public class Reference
 {
     public static final String MOD_ID = "itemscroller";
-    public static final String MOD_NAME = "Item Scroller";
-    public static final String MOD_VERSION = "@MOD_VERSION@";
+    public static final String MOD_NAME = "Item Scroller (Andrew fork)";
+    public static final String MOD_VERSION = StringUtils.getModVersionString(MOD_ID);
 }
